e17e2945aef2a84c247e3a4d1ca6f2db2eb2975d
lucapolzone <lucapolzone@gmail.com>
2024-01-24 17:35:14 +0100

    feat: add bonus 1

--- js/main.js
+++ js/main.js
@@ -4,9 +4,14 @@ const mainContainer = document.querySelector(".container")
 
 for (let i = 0; i < team.length; i++) {
   let member = team[i]; 
-  const description = `<p>Il suo nome è ${member.name}, il suo ruolo è ${member.role}. Ecco la stringa della sua foto: '${member.image}'</p>`
-  mainContainer.innerHTML += description
-  console.log(typeof description)
+  const description = `<p>Il suo nome è ${member.name}, il suo ruolo è ${member.role}`
+  
+  const image = `<img src="./img/${member.image}"></p>`
+
+  console.log(image)
+
+  mainContainer.innerHTML += description + image;
+  
 }
 
 
