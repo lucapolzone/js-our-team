1f33de0738f8e464dc723bce4565d4ec7f92e782
lucapolzone <lucapolzone@gmail.com>
2024-01-24 17:25:40 +0100

    feat: add milestone 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 // Ciclo per iterare attraverso l'array e stampare le informazioni per ogni membro del team
 
+const mainContainer = document.querySelector(".container")
+
 for (let i = 0; i < team.length; i++) {
-  const member = team[i];
-  console.log(`Il suo nome è ${member.name}, il suo ruolo è ${member.role}. Ecco la stringa della sua foto: '${member.image}'`)
+  let member = team[i]; 
+  const description = `<p>Il suo nome è ${member.name}, il suo ruolo è ${member.role}. Ecco la stringa della sua foto: '${member.image}'</p>`
+  mainContainer.innerHTML += description
+  console.log(typeof description)
 }
+
+
